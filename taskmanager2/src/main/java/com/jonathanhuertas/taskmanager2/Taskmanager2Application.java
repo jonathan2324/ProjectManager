0f328603dd98073949e60b60f12d88d5e3444b4b
Jonathan Huertas <jonathanhuertas@Jonathans-MacBook-Pro-2.local>
2020-06-07 10:43:17 -0500

    First commit

---
+++ taskmanager2/src/main/java/com/jonathanhuertas/taskmanager2/Taskmanager2Application.java
@@ -0,0 +1,13 @@
+package com.jonathanhuertas.taskmanager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taskmanager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taskmanager2Application.class, args);
+	}
+
+}
